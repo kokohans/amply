7226716ae07016d296d660f4916111b2963495bc
kokohans <hw.wiranata@gmail.com>
2021-08-15 19:28:50 +0700

    add mongoose connection settings

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 8000;
+const DB_URL = process.env.DB_AMPLY;
+
+const db = mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 
 const posts_controller = require("./controllers/posts_controller");
 const users_controller = require("./controllers/users_controller");
