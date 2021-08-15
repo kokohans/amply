349aecd913a8b625bcc77e3efa533b81bd7b7d80
kokohans <hw.wiranata@gmail.com>
2021-08-15 20:53:45 +0700

    fixing field desc to description

--- src/models/user_model.js
+++ src/models/user_model.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const userModel = new Schema({
   username: { type: String, unique: true, require: true },
   email: { type: String, unique: true, require: true },
-  desc: { type: String },
+  description: { type: String },
 });
 
 module.exports = mongoose.model("user", userModel, "users");
