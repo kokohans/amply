cefcc411e1b1ff1c26aca11230e4a7e631092c7e
kokohans <hw.wiranata@gmail.com>
2021-08-16 22:07:01 +0700

    add function list all posts

--- src/controllers/posts_controller.js
+++ src/controllers/posts_controller.js
@@ -1,10 +1,21 @@
 const router = require("express").Router();
+const Post = require("../models/post_model");
 
-router.get("/", (req, res) => {
-  res.json({
-    message: "hello world",
-    err: null,
+const get_posts = (req, res) => {
+  Post.find((err, post) => {
+    if (err) {
+      return res.json({
+        message: null,
+        err: true,
+      });
+    }
+    res.json({
+      message: post,
+      err: null,
+    });
   });
-});
+};
+
+router.get("/", get_posts);
 
 module.exports = router;
--- test/controllers/posts.test.js
+++ test/controllers/posts.test.js
@@ -4,28 +4,70 @@ const expect = chai.expect;
 
 chai.use(chaiHTTP);
 
+const User = require("../../src/models/user_model");
+const Post = require("../../src/models/post_model");
+
 let server = require("../../src/app");
 
 describe("Post Controller", () => {
+  before((done) => {
+    User.deleteMany({});
+    Post.deleteMany({});
+
+    let uid = "";
+    let user_data = {
+      username: "kokohan_post",
+      email: "hans_post@gmail.com",
+      description: "this is my personal space",
+    };
+
+    let new_user = new User(user_data);
+    new_user.save((err, user) => {
+      uid = user["_id"];
+
+      let post_data = {
+        body: "this is hans's tweet",
+        comments: [],
+        user: uid,
+        created_at: new Date().toISOString(),
+      };
+
+      let new_post = new Post(post_data);
+
+      new_post.save((err) => {
+        done();
+      });
+    });
+  });
+
+  after(() => {
+    User.deleteMany({});
+    Post.deleteMany({});
+  });
+
   describe("GET /api/v1/posts", () => {
-    it("should return 200 OK", (done) => {
+    let post_url = "/api/v1/posts";
+    let response, error;
+
+    before((done) => {
       chai
         .request(server)
-        .get("/api/v1/posts")
+        .get(post_url)
         .end((err, res) => {
-          expect(res.statusCode).to.equals(200);
+          error = err;
+          response = res;
           done();
         });
     });
 
-    it("should return response with JSON", (done) => {
-      chai
-        .request(server)
-        .get("/api/v1/posts")
-        .end((err, res) => {
-          expect(res.body).to.have.property("message");
-          done();
-        });
+    it("should return 200 OK", () => {
+      expect(response.statusCode).to.equals(200);
+    });
+
+    it("should return response with JSON", () => {
+      expect(response.body).to.have.property("message");
+      expect(response.body["message"]).to.be.an.an("array");
+      expect(response.body["err"]).to.be.null;
     });
   });
 });
